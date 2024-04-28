9fbc2fabdc866faf0daffe34c58d9771d8970a67
michael-carvajal <michaelcarvajal.work@gmail.com>
2024-04-28 12:53:28 -0400

    added navbar icons

--- App.tsx
+++ App.tsx
@@ -35,9 +35,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
+        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpPage} options={{ headerShown: false }} />
         <Stack.Screen name="Login" component={LoginPage} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={Home} options={{ headerShown: false }} />
         <Stack.Screen name="TermsAndConditions" component={TermsAndConditionsPage} options={{ headerShown: true }} />
         <Stack.Screen name="PrivacyPolicy" component={PrivacyPolicyPage} options={{ headerShown: true }} />
       </Stack.Navigator>
--- app/home/Home.tsx
+++ app/home/Home.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
+import { View, StyleSheet, Image } from 'react-native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import ProfilePage from '../profile/Profile';
 import Coach from '../coach/Coach';
@@ -7,22 +7,66 @@ import Play from '../play/Play';
 import Tournament from '../tournament/Tournament';
 import Social from '../social/Social';
 
+// Import your tab icons
+import profileIcon from '../assets/images/profile-icon.png';
+import playIcon from '../assets/images/play-icon.png';
+import tournamentIcon from '../assets/images/tournament-icon.png';
+import coachIcon from '../assets/images/coach-icon.png';
+import socialIcon from '../assets/images/social-icon.png';
+
 const Tab = createBottomTabNavigator();
-const Home = ({ navigation } :any) => {
-  const handleTabChange = (tabName: String) => {
+const Home = ({ navigation }: any) => {
+  const handleTabChange = (tabName: string) => {
     // Handle tab change here
     console.log('Switched to tab:', tabName);
   };
 
   return (
     <View style={styles.container}>
-    <Tab.Navigator>
-      <Tab.Screen name="Profile" component={ProfilePage} />
-      <Tab.Screen name="Play" component={Play} />
-      <Tab.Screen name="Tournament" component={Tournament} />
-      <Tab.Screen name="Coach" component={Coach} />
-      <Tab.Screen name="Social" component={Social} />
-    </Tab.Navigator>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarStyle: { backgroundColor: '#007AFF', height: 100 },
+          tabBarShowLabel: false,
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconSource;
+
+            // Define the icon source based on the route name
+            switch (route.name) {
+              case 'Profile':
+                iconSource = profileIcon;
+                break;
+              case 'Play':
+                iconSource = playIcon;
+                break;
+              case 'Tournament':
+                iconSource = tournamentIcon;
+                break;
+              case 'Coach':
+                iconSource = coachIcon;
+                break;
+              case 'Social':
+                iconSource = socialIcon;
+                break;
+              default:
+                // Default icon or placeholder
+                iconSource = null;
+            }
+            const tournamentSize = route.name === 'Tournament' ? { width: 100, height: 100 } : { width: 64
+              , height: 64
+              
+             };
+            // Return the image component
+            return <Image source={iconSource} style={{ ...tournamentSize }} />;
+          },
+
+        })}
+      >
+        <Tab.Screen name="Profile" component={ProfilePage} />
+        <Tab.Screen name="Play" component={Play} />
+        <Tab.Screen name="Tournament" component={Tournament} />
+        <Tab.Screen name="Coach" component={Coach} />
+        <Tab.Screen name="Social" component={Social} />
+      </Tab.Navigator>
     </View>
   );
 };
@@ -31,14 +75,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     justifyContent: 'flex-end', // Align bottom
-  },
-  tabContainer: {
-    flexDirection: 'row',
-    backgroundColor: '#fff', // Change this to your desired background color
-    justifyContent: 'space-around',
-  },
-  selectedTab: {
-    backgroundColor: '#ddd', // Change this to your desired selected tab background color
+    backgroundColor: 'lightblue'
   },
 });
 
